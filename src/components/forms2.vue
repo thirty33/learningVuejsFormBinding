496d3bffaa74dd08ba35d615b29bd4b3f289a0e0
thirty33 <joelsuarez.1101@gmail.com>
2017-11-18 05:29:45 -0500

    asociando valores a selects o checkbox con vuejs

---
+++ src/components/forms2.vue
@@ -0,0 +1,44 @@
+<template>
+	<div>
+		
+		<h1>prueba 1</h1>
+		<input type="checkbox" v-model="checkbox" :true-value="a" :false-value="b">
+		<hr>
+			
+		<!-- radio	 -->
+
+		<input type="radio" v-model="radio" :value="a">
+
+		<!-- select -->
+
+		<select v-model="select">
+			<option :value="a">Juan</option>
+			<option :value="a">pedro</option>
+		</select>
+
+		<pre>{{ $data }}</pre>		
+
+	</div>
+	
+</template>
+
+
+<script>
+	export default {
+		data() {
+			return {
+				checkbox: null,
+				a: {estado: 'checked'},
+				b: {estado: 'unchecked'},
+				radio: null,
+				select: null
+
+			}
+		}
+	}
+</script>
+
+<style>
+	
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import forms2 from '@/components/forms2'
 
 Vue.use(Router)
 
 export default new Router({
+	mode: 'history',		
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+    	path: '/form',
+    	name: 'form',
+    	component: forms2
     }
   ]
 })
