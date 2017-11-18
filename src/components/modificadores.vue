cf389c63950231e7b949ef5299e7d3dd5b2fdd3b
thirty33 <joelsuarez.1101@gmail.com>
2017-11-18 09:03:47 -0500

    modificadores con v-model

---
+++ src/components/modificadores.vue
@@ -0,0 +1,47 @@
+<template>
+	<div>
+		<h1>modificadores</h1>
+		<form @submit.prevent="enviar">
+			<!--lazy, espera al evento submit -->
+			<input type="text" v-model.lazy="formulario">
+
+			<!-- number, pasa a elemento tipo number -->
+			<input type="number" v-model.number="formnum">
+
+			<!-- trin, quitar espacios -->
+			<input type="text" v-model.trim="trin">
+
+	
+			<input type="submit" value="enviar">
+		</form>
+		<hr>
+
+		<pre> {{ $data }}</pre>
+	</div>
+</template>
+
+
+<script>
+	export default {
+		data(){
+			return {
+				formulario: null,
+				formnum: null,
+				trin: null
+			}
+		},
+		methods: {
+			enviar(){
+				console.info(typeof this.formnum)
+				console.info(typeof this.trin)
+
+			}
+		}
+	}
+</script>
+
+
+<style>
+	
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import forms2 from '@/components/forms2'
+import modificadores from '@/components/modificadores'
 
 Vue.use(Router)
 
@@ -17,6 +18,12 @@ export default new Router({
     	path: '/form',
     	name: 'form',
     	component: forms2
+    },
+    {
+    	path: '/modis',
+    	name: 'modis',
+    	component: modificadores,
+
     }
   ]
 })
